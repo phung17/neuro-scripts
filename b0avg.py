3388d5c4bdbeb5f10426c4bf818bdbfc859d9f4d
Peter SP Hung <phung17@outlook.com>
2015-12-14 13:21:33 -0500

    Revert b0avg.py to Master branch version, edge version has a list error

--- b0avg.py
+++ b0avg.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 from glob import glob
 from optparse import OptionParser
 import os
 import sys
 import numpy
-from pynrrd import NrrdReader
+from nrrd import NrrdReader
 
 def getOrderStr(ax, num):
     a = numpy.zeros(4, dtype=numpy.int)
@@ -13,21 +13,21 @@ def getOrderStr(ax, num):
     return ' '.join(str(i) for i in a)
 
 class B0avg:
-   
+
     def __init__(self, options, args):
         self.options = options
         self.args = args
 
 
     def run(self):
-        
+
         ifile = self.options.input
         ofile = self.options.output
-        
+
         reader = NrrdReader()
-        iparams, bindata =  reader.load(ifile)
+        iparams, bindata =  reader.getFileContent(ifile)
 	#iparams = iparams._data
-        
+
         b0n = iparams['b0num']
         print "%d b0 to be averaged" % b0n
         b0n -= 1
@@ -39,26 +39,26 @@ class B0avg:
 
         if options.is_average:
             cmd = "unu crop -i %s -min %s -max M M M M -o tmp.nhdr; " % ( ifile, difcropstr)
-            cmd += "unu crop -i %s -min 0 0 0 0 -max %d M M M | unu project -a %s -m mean | unu splice -i grad.nhdr -s - -a 0 -p 0 -o %s; rm tmp.*" % (ifile, b0n, options.aorder, ofile)        
+            cmd += "unu crop -i %s -min 0 0 0 0 -max %d M M M | unu project -a %s -m mean | unu splice -i grad.nhdr -s - -a 0 -p 0 -o %s; rm tmp.*" % (ifile, b0n, options.aorder, ofile)
         else:
             cmd = "unu crop -i %s -min %s -max M M M M -o %s " % ( ifile, orderstr, ofile)
 
-        print cmd        
+        print cmd
         os.system(cmd)
-        
-        
+
+
         ofilein = open(ofile,'r')
         content = []
         while ofilein:
-            line = ofilein.readline()            
+            line = ofilein.readline()
             if len(line) == 0:
                 break;
             if line.startswith(NrrdReader.grdkey):
                 continue
             content.append(line)
         ofilein.close()
-        
-        
+
+
         count = 0
         for n,i in enumerate(iparams[NrrdReader.grdkey]):
             if count > 0 and n <= b0n:
@@ -69,14 +69,14 @@ class B0avg:
                 oline += '%1.6f ' % j
             content.append(oline+'\n')
             count+=1
-            
+
             ofileout = open(ofile, 'w')
             for l in content:
                 if options.verbose:
                     print l
                 ofileout.write(l)
             ofileout.close()
-              
+
 
 
 if __name__ == '__main__':
@@ -84,8 +84,8 @@ if __name__ == '__main__':
     parser.add_option("-i", "--input", dest="input", help="input .nhdr file to be averaged")
     parser.add_option("-o", "--output", dest="output", default="b0averaged.nhdr", help="output file name")
     parser.add_option("-a", "--array_order", dest="aorder", type="int", default="0", help="Index position of gradient array in the MR matrix, default is 0 for (1,0,0,0)")
-    parser.add_option("-g", "--is_average", dest="is_average", action='store_true', default=False, help="Average the b0s rather than discarding all but the last one")    
-    parser.add_option("-v", "--verbose", dest="verbose", action='store_true', default=False, help="print out all debug messages")    
+    parser.add_option("-g", "--is_average", dest="is_average", action='store_true', default=False, help="Average the b0s rather than discarding all but the last one")
+    parser.add_option("-v", "--verbose", dest="verbose", action='store_true', default=False, help="print out all debug messages")
 
     (options, args) = parser.parse_args()
 
